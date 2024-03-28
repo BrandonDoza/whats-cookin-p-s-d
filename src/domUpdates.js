f1d5db8f5e5065b1bcdaba9bcde40e5e01291251
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-03-27 20:50:14 -0600

    feat: Will add a landing page event that hides and unhides associated elements.

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,4 +1,14 @@
 //NOTE: Your DOM manipulation will occur in this file
+const landingPage = document.querySelector('page-load')
+const navBar = document.querySelector('after-load-side-bar hidden')
+const homePage = document.querySelector('after-load-main-view hidden')
+document.addEventListener('DOMContentLoaded', () => {
+ setTimeout(()=>{
+    landingPage.classList.add('hidden')
+    navBar.classList.remove('hidden')
+    homePage.classList.remove('hidden')
+ },300)
+});
 
 //Here is an example function just to demonstrate one way you can export/import between the two js files. You'll want to delete this once you get your own code going.
 const displayRecipes = () => {
