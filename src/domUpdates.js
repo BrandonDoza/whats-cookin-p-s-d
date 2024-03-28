e700364e73f081d4c4dcecc21bbc6294259ae95c
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-03-27 22:38:34 -0600

    fix: query selectors now access the expected elements.

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,21 +1,33 @@
+import { recipes , ingredients} from "../test/mock-data"
+
 //NOTE: Your DOM manipulation will occur in this file
-const landingPage = document.querySelector('page-load')
-const navBar = document.querySelector('after-load-side-bar hidden')
-const homePage = document.querySelector('after-load-main-view hidden')
-document.addEventListener('DOMContentLoaded', () => {
+
+const landingPage = document.querySelector('.page-load')
+const navBar = document.querySelector('.after-load-side-bar')
+const navBarTags = document.querySelector('.tag-view-search')
+const homePage = document.querySelector('.after-load-main-view')
+
+document.addEventListener('DOMContentLoaded', function(){
  setTimeout(()=>{
     landingPage.classList.add('hidden')
     navBar.classList.remove('hidden')
     homePage.classList.remove('hidden')
- },300)
+ },3000)
 });
+navBar.addEventListener('click', (event) =>{
+  
+  let element = event.target
+  if (element.classList.contains('tags')){
+      navBar.classList.add('hidden')
+    }
+  });
 
 //Here is an example function just to demonstrate one way you can export/import between the two js files. You'll want to delete this once you get your own code going.
-const displayRecipes = () => {
-  console.log(`Displaying recipes now`)
-}
+// const displayRecipes = () => {
+//   console.log(`Displaying recipes now`)
+// }
 
 
-export {
-  displayRecipes,
-}
+// export {
+//   displayRecipes,
+// }
