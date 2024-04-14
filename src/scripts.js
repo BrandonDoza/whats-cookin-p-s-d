745cdcdaacf8b6a1b160b3e360ce9bb5c05b3087
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-04-14 16:01:32 -0600

    feat: Will add a currency conversion table.

--- src/scripts.js
+++ src/scripts.js
@@ -18,3 +18,16 @@ export function updateRecipeDataModel(searchResult) {
   dataModel.currentRecipes = searchResult;
 }
 
+export const currencies = [
+  {name: 'USD', conversionRate: 1.00},
+  {name: 'CAD', conversionRate: 1.38},
+  {name: 'GBP', conversionRate: 0.80},
+  {name: 'CAD', conversionRate: 0.94},
+  {name: 'swedishKrona', conversionRate: 10.88},
+  {name: 'japaneseYen', conversionRate: 153.28},
+  {name: 'chineseYuan', conversionRate: 7.24},
+  {name: 'australianDollar', conversionRate: 1.54},
+  {name: 'indianRupee', conversionRate: 83.61},
+  {name: 'hatianGourde', conversionRate: 132.27},
+];
+
